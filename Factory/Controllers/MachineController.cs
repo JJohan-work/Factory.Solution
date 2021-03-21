786701157004bf8406bf3f2100fd98b470fc405b
Jonah Johansen <johansenjonah+git@gmail.com>
2021-03-21 12:25:02 -0700

    add machine creation

--- Factory/Controllers/MachineController.cs
+++ Factory/Controllers/MachineController.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Factory.Models;
 using System.Collections.Generic;
+using System;
 using System.Linq;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc.Rendering;
@@ -18,6 +19,23 @@ namespace Factory.Controllers
     public ActionResult Index()
     {
       return View(_db.Machines.ToList());
+    }
+
+    public ActionResult Create()
+    {
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Machine machine)
+    {
+      if (machine.DateInstalled.Year == 0001)
+      {
+        machine.DateInstalled = DateTime.Now;
+      }
+      _db.Machines.Add(machine);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
     }
   }
 }
